129601a2c855438dcba3529941572d0a81be35ce
k-ramaswamy <k.ramaswamy@salesforce.com>
2021-06-22 12:54:27 -0500

    Exercise 2-4 Completed

---
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,14 @@
+import { LightningElement, api } from 'lwc';
+
+export default class StudentTile extends LightningElement {
+
+    @api isSelected = false;
+    @api student = {
+        Name: 'Karthik',
+        PhotoUrl: '/services/images/photo/003B0FakePictId',
+        };
+
+        get tileSelected() {
+            return this.isSelected ? "tile selected" : "tile";
+        }
+}
